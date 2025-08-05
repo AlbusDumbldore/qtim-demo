3b8e23908975cc365cfc83a0b4a25bcab39a58d5
Sergey Sorokin <albuspersiv19@gmail.com>
2025-08-05 08:49:22 +0300

    fix: Исправлен DatabaseModule

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -2,15 +2,14 @@ import { Global, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import appConfig from '../config';
 
-const typeormModule = TypeOrmModule.forRoot({
-  ...appConfig.postgres,
-  type: 'postgres',
-  entities: [__dirname + '/**/*.entity{.ts,.js}'],
-});
-
 @Global()
 @Module({
-  imports: [typeormModule],
-  exports: [typeormModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      ...appConfig.postgres,
+      type: 'postgres',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    }),
+  ],
 })
 export class DatabaseModule {}
